6679bcf92a6f85ec69a830aceb9de21adccecad4
jeffreywastaken <jeffreyixora@gmail.com>
2023-08-04 03:21:47 +0400

    slight tests being done

---
+++ geofence.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDyoUGXsujgMI9yvLftNDneZweyWZSeb8s",
+    authDomain: "empower-naija.firebaseapp.com",
+    projectId: "empower-naija",
+    storageBucket: "empower-naija.appspot.com",
+    messagingSenderId: "31519409443",
+    appId: "1:31519409443:web:397a432b4ec9c19a0db896",
+    measurementId: "G-CN0F62EFT3"
+  };
+  
+  
+  
+ // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
